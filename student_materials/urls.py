f000bf50efd4b5314ed9ae483a20d5474634eb59
ngattt82 <55889654+ngattt82@users.noreply.github.com>
2024-09-30 20:00:57 +0700

    update material

--- student_materials/urls.py
+++ student_materials/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('materials/download/all/<str:material_type>/', views.download_all_materials, name='download_all_materials'),
     path('materials/view/<int:material_id>/', views.view_material, name='view_material'),
     path('view_pdf/<path:google_drive_link>/', views.view_pdf, name='view_pdf'),
-    
+    path('view_material/<int:material_id>/', views.view_material, name='view_material'),  # Change id to mat
     # AJAX endpoints
     path('api/training_programs/<int:training_program_id>/subjects/', views.get_subjects, name='get_subjects'),
     path('api/subjects/<int:subject_id>/material_types/', views.get_material_types, name='get_material_types'),
